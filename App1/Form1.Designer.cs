a45b0209119070f8e9cba26c59addfa153afd218
Renaldi15120 <43535476+Renaldi15120@users.noreply.github.com>
2018-09-27 12:45:36 +0700

    Add files via upload

---
+++ App1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace App1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.hasil = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Hitung = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // combo
+            // 
+            this.combo.FormattingEnabled = true;
+            this.combo.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.combo.Location = new System.Drawing.Point(151, 15);
+            this.combo.Name = "combo";
+            this.combo.Size = new System.Drawing.Size(121, 21);
+            this.combo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Oprasi";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Niali A";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Nilai B";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(151, 68);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 4;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(151, 124);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 5;
+            // 
+            // hasil
+            // 
+            this.hasil.Location = new System.Drawing.Point(151, 172);
+            this.hasil.Name = "hasil";
+            this.hasil.Size = new System.Drawing.Size(100, 20);
+            this.hasil.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "hasil";
+            // 
+            // Hitung
+            // 
+            this.Hitung.Location = new System.Drawing.Point(16, 226);
+            this.Hitung.Name = "Hitung";
+            this.Hitung.Size = new System.Drawing.Size(75, 23);
+            this.Hitung.TabIndex = 8;
+            this.Hitung.Text = "Hitung";
+            this.Hitung.UseVisualStyleBackColor = true;
+            this.Hitung.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 261);
+            this.Controls.Add(this.Hitung);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hasil);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox combo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox hasil;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Hitung;
+    }
+}
+
---
+++ App1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(txtA.Text);
+            var b = Convert.ToInt32(txtB.Text);
+            Double lsthasil;
+            switch (combo.SelectedIndex)
+            {
+                case 0:
+                    lsthasil = a + b;
+                    hasil.Text = Convert.ToString(lsthasil);
+                    break;
+                case 1:
+                    lsthasil = a - b;
+                    hasil.Text = Convert.ToString(lsthasil);
+                    break;
+                case 2:
+                    lsthasil = a * b;
+                    hasil.Text = Convert.ToString(lsthasil);
+                    break;
+                case 3:
+                    lsthasil = a / b;
+                    hasil.Text = Convert.ToString(lsthasil);
+                    break;
+            }
+        }
+    }
+}
